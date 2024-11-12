b2e498c7bbdfa5d7f48596980068678c86aac733
Dreammarya <marialazar3@gmail.com>
2024-11-12 15:52:12 +0100

    commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+def load_data(animals_data.json):
+  """ Loads a JSON file """
+  with open( animals_data.json):
+  """ Loads a JSON file """
+  with open( animals_data.json):
+  """ Loads a JSON file """
+  with open(animals_data.json, "r") as handle:
+    return json.load(handle)
+, "r") as handle:
+    return json.load(handle), "r") as handle:
+    return json.load(handle)
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
